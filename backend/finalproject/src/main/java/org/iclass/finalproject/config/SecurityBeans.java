d7f135d484ab8c398d42ddc64b4d9fec065a0001
Park Nohjun <wonpoo999@naver.com>
2025-11-20 15:19:56 +0900

    백엔드 작업중

---
+++ backend/finalproject/src/main/java/org/iclass/finalproject/config/SecurityBeans.java
@@ -0,0 +1,16 @@
+// src/main/java/org/class/finalproject/config/SecurityBeans.java
+package org.iclass.finalproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityBeans {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
