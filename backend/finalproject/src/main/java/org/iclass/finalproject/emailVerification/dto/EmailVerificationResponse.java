6926d5bf793ce2fd648943d44bb841ca939abcf4
seona <ghdtjsdk312@gmail.com>
2025-11-21 11:18:48 +0900

    이메일 인증 로직 잠시 주석 처리

--- backend/finalproject/src/main/java/org/iclass/finalproject/emailVerification/dto/EmailVerificationResponse.java
+++ backend/finalproject/src/main/java/org/iclass/finalproject/emailVerification/dto/EmailVerificationResponse.java
@@ -1,35 +1,35 @@
-package org.iclass.finalproject.emailVerification.dto;
+// package org.iclass.finalproject.emailVerification.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+// import lombok.AllArgsConstructor;
+// import lombok.Builder;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class EmailVerificationResponse {
+// @Getter
+// @Setter
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @Builder
+// public class EmailVerificationResponse {
     
-    private boolean success;
-    private String message;
-    private String email;
+//     private boolean success;
+//     private String message;
+//     private String email;
     
-    public static EmailVerificationResponse success(String email) {
-        return EmailVerificationResponse.builder()
-                .success(true)
-                .message("이메일 인증이 완료되었습니다")
-                .email(email)
-                .build();
-    }
+//     public static EmailVerificationResponse success(String email) {
+//         return EmailVerificationResponse.builder()
+//                 .success(true)
+//                 .message("이메일 인증이 완료되었습니다")
+//                 .email(email)
+//                 .build();
+//     }
     
-    public static EmailVerificationResponse failure(String message) {
-        return EmailVerificationResponse.builder()
-                .success(false)
-                .message(message)
-                .email(null)
-                .build();
-    }
-}
+//     public static EmailVerificationResponse failure(String message) {
+//         return EmailVerificationResponse.builder()
+//                 .success(false)
+//                 .message(message)
+//                 .email(null)
+//                 .build();
+//     }
+// }
