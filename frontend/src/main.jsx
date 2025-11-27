8eaea5278c6eff5bef363ab6453a3afa253ab8c8
GAMGO <gamgo445@gmail.com>
2025-11-27 16:55:01 +0900

    로그인하면 들어가짐

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import './index.css'
 import App from "./App.jsx";
 import AuthPage from "./pages/AuthPage.jsx";
-import Main from "./components/Main";
+import Check from "./pages/AuthCheck.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<AuthPage />} />
-        <Route path="/" element={<App />} />
+        <Route path="/app" element={<App />} />
+        <Route path="/" element={<Check />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
