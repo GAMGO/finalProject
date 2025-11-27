64eed862820d55d065a2d472929c9b509123da7a
GAMGO <gamgo445@gmail.com>
2025-11-27 19:43:19 +0900

    ㅁㄴㅇㅁㄴㅊㅍㅈㅁㄷㅍㅁㄷㅍㅁㄴㅇㄻㄷㄹㅋㄴㅇㅍㅁㄴㄷㅁㄴㄷㄿㅁㄴㄷㅍ

--- backend/src/main/java/org/iclass/favorite/repository/FavoriteRepository.java
+++ backend/src/main/java/org/iclass/favorite/repository/FavoriteRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface FavoriteRepository extends JpaRepository<FavoriteEntity, Long> {
 
-    List<FavoriteEntity> findByCustomerIdOrderByCreatedAtDesc(Long customer_idx);
+    List<FavoriteEntity> findByCustomerIdOrderByCreatedAtDesc(Long idx);
 }
