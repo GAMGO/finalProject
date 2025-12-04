7db53e6de4b08c165a79792449069cb743444104
GAMGO <gamgo445@gmail.com>
2025-12-04 11:56:19 +0900

    lol

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -5,7 +5,6 @@ import SignupPage from "./SignupPage";
 import EmailAuth from "./EmailAuth";
 const baseURL = import.meta.env.VITE_BASE_URL;
 const AuthPage = ({ onLoginSuccess }) => {
-  const navigate = useNavigate();
   // 🌟 이 상태가 로그인/회원가입 모드를 결정합니다.
   const [authMode, setAuthMode] = useState('login');
   const [registeredEmail, setRegisteredEmail] = useState('');
