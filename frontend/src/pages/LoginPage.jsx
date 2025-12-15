2bcff65f641aa7b4874edd395b3595998c31040d
Minsu Lim <gamgo445@gmail.com>
2025-12-16 07:53:51 +0900

    css

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -174,12 +174,6 @@ const handleLogin = async () => {
     fontFamily: customFont,
   };
 
-  const logoContainerStyle = {
-    maxWidth: "100%",
-    height: "auto",
-    marginBottom: "30px",
-  };
-
   const inputGroupStyle = {
     marginBottom: "20px",
     textAlign: "left",
@@ -232,10 +226,11 @@ const handleLogin = async () => {
   };
   const logo_img = {
   display: block,
-  width: "120px",
+  maxWidth: "100%",
   height: "auto",
   border: "none !important",
-  outline: "none !important"
+  outline: "none !important",
+  marginBottom: "30px",
 };
   const messageStyle = {
     marginTop: "15px",
