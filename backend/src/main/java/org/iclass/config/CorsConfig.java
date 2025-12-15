0eccd2275882bbf6fdeb92c9c6fa3b514c2bdde0
Minsu Lim <gamgo445@gmail.com>
2025-12-16 06:45:48 +0900

    https://finalproject-production-c135.up.railway.app

--- backend/src/main/java/org/iclass/config/CorsConfig.java
+++ backend/src/main/java/org/iclass/config/CorsConfig.java
@@ -20,6 +20,7 @@ public class CorsConfig {
         c.addAllowedOriginPattern("http://localhost:8080");
         c.addAllowedOriginPattern("http://127.0.0.1:8000");
         c.addAllowedOriginPattern("https://finalproject-railway.app");
+        c.addAllowedOriginPattern("https://finalproject-production-c135.up.railway.app");
         c.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         c.addAllowedHeader("*");
         c.setAllowedHeaders(List.of("Content-Type", "Authorization"));
